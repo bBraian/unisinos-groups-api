4aec2db2e6dc1db1ac7335f4f1dbcc219c037662
bBraian <braianvoficial@gmail.com>
2024-06-03 20:58:54 -0300

    feat: add feedback

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,11 @@ import { AccountController } from './account/account.controller';
 import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { CoursesModule } from './courses/courses.module';
+import { FeedbackModule } from './feedback/feedback.module';
+import { FeedbackModule } from './feedback/feedback.module';
 
 @Module({
-  imports: [PrismaModule, AuthModule, CoursesModule],
+  imports: [PrismaModule, AuthModule, CoursesModule, FeedbackModule],
   controllers: [AccountController],
   providers: [],
 })
