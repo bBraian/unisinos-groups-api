e7c897e6022f0b91ee5a3db4c0e30a246d52ed46
bBraian <braianvoficial@gmail.com>
2024-06-03 21:27:00 -0300

    feat: add feedback

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { PrismaModule } from './prisma/prisma.module';
 import { CoursesModule } from './courses/courses.module';
 import { FeedbackModule } from './feedback/feedback.module';
 import { FeedbackModule } from './feedback/feedback.module';
+import { FeedbackModule } from './feedback/feedback.module';
 
 @Module({
   imports: [PrismaModule, AuthModule, CoursesModule, FeedbackModule],
