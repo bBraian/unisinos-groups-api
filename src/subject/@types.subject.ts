f8ac2a9c8a8d7cb21678676c9daf8c25f62983cd
bBraian <braianvoficial@gmail.com>
2024-06-11 21:09:15 -0300

    feat: update zod validation

--- src/subject/@types.subject.ts
+++ src/subject/@types.subject.ts
@@ -1,15 +1,15 @@
 import { z } from "zod";
 
 const createLinkBodySchema = z.object({
-  title: z.string(),
-  link: z.string(),
-  type: z.string(),
+  title: z.string().trim().min(1, { message: "Required" }),
+  link: z.string().trim().min(1, { message: "Required" }),
+  type: z.string().trim().min(1, { message: "Required" })
 })
 
 export const createSubjectBodySchema = z.object({
-  course: z.number(),
-  title: z.string(),
-  image: z.string(),
+  course: z.number().min(1, { message: "Required" }),
+  title: z.string().trim().min(1, { message: "Required" }),
+  image: z.string().optional(),
   whatsappLinks: z.array(createLinkBodySchema),
   driveLinks: z.array(createLinkBodySchema)
 })
