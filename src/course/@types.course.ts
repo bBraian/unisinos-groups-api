6aeb035fe34b98c38ea65ee78e067d7e6089268d
bBraian <braianvoficial@gmail.com>
2024-06-11 22:00:39 -0300

    feat: required fields zod validation

--- src/course/@types.course.ts
+++ src/course/@types.course.ts
@@ -1,7 +1,7 @@
 import { z } from "zod";
 
 export const createCourseBodySchema = z.object({
-  name: z.string()
+  name: z.string().trim().min(1, { message: "Required" }),
 })
 
 export type CreateCourseBodySchema = z.infer<typeof createCourseBodySchema>
--- src/link/@types.type.ts
+++ src/link/@types.type.ts
@@ -1,18 +1,18 @@
 import { z } from "zod";
 
 export const createLinkBodySchema = z.object({
-  title: z.string(),
-  link: z.string(),
-  type: z.string(),
+  title: z.string().trim().min(1, { message: "Required" }),
+  link: z.string().trim().min(1, { message: "Required" }),
+  type: z.string().trim().min(1, { message: "Required" }),
   subjectId: z.number()
 })
 
 export type CreateLinkBodySchema = z.infer<typeof createLinkBodySchema>
 
 export const updateLinkBodySchema = z.object({
-  title: z.string(),
-  link: z.string(),
-  type: z.string(),
+  title: z.string().trim().min(1, { message: "Required" }),
+  link: z.string().trim().min(1, { message: "Required" }),
+  type: z.string().trim().min(1, { message: "Required" }),
   subjectId: z.number()
 })
 
